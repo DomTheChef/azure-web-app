4efd13c11a591b849d37e1031f9787637a32948b
Dominic Senarsky <dominico.sebastian@gmail.com>
2025-10-06 13:21:52 +0200

    cleanup and aligning discrepancies such as func name and removing unncessary imports

--- function_app.py
+++ function_app.py
@@ -2,7 +2,6 @@ import azure.functions as func
 import os
 import uuid
 from datetime import datetime, timezone
-import json
 from azure.cosmos import CosmosClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
@@ -15,7 +14,7 @@ client = CosmosClient.from_connection_string(COSMOS_CONN)
 database = client.get_database_client(DB_NAME)
 container = database.get_container_client(CONTAINER_NAME)
 
-@app.function_name(name="VisitorCounter")
+@app.function_name(name="track_visit")
 @app.route(route="track", methods=["POST"])
 def track_visit(req: func.HttpRequest) -> func.HttpResponse:
     try:
@@ -24,7 +23,6 @@ def track_visit(req: func.HttpRequest) -> func.HttpResponse:
             "timestamp": datetime.now(timezone.utc).replace(microsecond=0).isoformat(),
             "ip": req.headers.get("x-forwarded-for", req.remote_addr)
         }
-
 
         container.create_item(body=visit)
 
