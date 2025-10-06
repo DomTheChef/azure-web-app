bb1d6ab9000e1137211a39941b9e790b910a2645
Dominic Senarsky <dominico.sebastian@gmail.com>
2025-10-06 16:51:20 +0200

    Removing any CosmosDB sources so as to confirm if that is RC

--- function_app.py
+++ function_app.py
@@ -6,14 +6,6 @@ from azure.cosmos import CosmosClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-COSMOS_CONN = os.environ["CosmosDBConnection"]   
-DB_NAME = "VisitCount"                     
-CONTAINER_NAME = "Visitors"                   
-
-client = CosmosClient.from_connection_string(COSMOS_CONN)
-database = client.get_database_client(DB_NAME)
-container = database.get_container_client(CONTAINER_NAME)
-
 @app.function_name(name="track_visit")
 @app.route(route="track")
 def track_visit(req: func.HttpRequest) -> func.HttpResponse:
@@ -22,8 +14,6 @@ def track_visit(req: func.HttpRequest) -> func.HttpResponse:
             "id": str(uuid.uuid4()),
             "timestamp": datetime.now(timezone.utc).replace(microsecond=0).isoformat()
         }
-
-        container.create_item(body=visit)
 
         return func.HttpResponse("Site visit recorded", status_code=200)
     except Exception as e:
