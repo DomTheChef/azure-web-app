3310a74cd4c0a08a39d80f80935445352a34c6c8
Dominic Senarsky <dominico.sebastian@gmail.com>
2025-10-07 15:05:43 +0200

    moved cosmosdb connection inside function to avoid funcapp deploy failure on import due to DB/network issues.

--- function_app.py
+++ function_app.py
@@ -1,6 +1,7 @@
 import azure.functions as func
 from azure.cosmos import CosmosClient
 import os
+import uuid
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -8,11 +9,19 @@ COSMOS_CONN = os.environ["CosmosDBConnection"]
 DB_NAME = "VisitCount"                     
 CONTAINER_NAME = "Visitors"                   
 
-client = CosmosClient.from_connection_string(COSMOS_CONN)
-database = client.get_database_client(DB_NAME)
-container = database.get_container_client(CONTAINER_NAME)
-
 @app.route(route="track", methods=["GET", "POST"])
 def track(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("OK", status_code=200)
+    try:
+        conn = os.environ["CosmosDBConnection"]
+        client = CosmosClient.from_connection_string(conn)
+        database = client.get_database_client(DB_NAME)
+        container = database.get_container_client(CONTAINER_NAME)
+
+        # Testing insert into Cosmos container..
+        item = {"id": str(uuid.uuid4()), "note": "testEntry"}
+        container.create_item(item)
+
+        return func.HttpResponse("Ok", status_code=200)
+    except Exception as e:
+        return func.HttpResponse(f"CosmosDB error: {e}", status_code=500)
 
