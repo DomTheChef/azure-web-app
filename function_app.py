ce2200320ce4d6272513c40c34b77856a8e74b1e
Dominic Senarsky <dominico.sebastian@gmail.com>
2025-10-07 22:46:53 +0200

    fix: binding parameter output needed to be string, then the output becomes json string

--- function_app.py
+++ function_app.py
@@ -1,22 +1,24 @@
 import azure.functions as func
 import uuid
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="track", methods=["GET", "POST"])
 @app.cosmos_db_output(
     arg_name="doc",
     database_name="VisitCount",
     container_name="Visitors",
-    connection="CosmosDBConnection"   
+    connection="CosmosDBConnection"
 )
-def track(req: func.HttpRequest, doc: func.Out[dict]) -> func.HttpResponse:
+@app.route(route="track", methods=["GET", "POST"])
+def track(req: func.HttpRequest, doc: func.Out[str]) -> func.HttpResponse:
     try:
         item = {
-            "id": str(uuid.uuid4()),   
+            "id": str(uuid.uuid4()),
             "note": "testEntry"
         }
-        doc.set(item)
+
+        doc.set(json.dumps(item))
 
         return func.HttpResponse("Ok", status_code=200)
     except Exception as e:
