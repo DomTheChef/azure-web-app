b3c4713c2642b6eaef19fe542869b7cd23d617c4
Dominic Senarsky <dominico.sebastian@gmail.com>
2025-10-07 15:43:46 +0200

    fix(cosmos connection): attempting fix on cosmos connection and fixed duplicate issue

--- function_app.py
+++ function_app.py
@@ -5,15 +5,13 @@ import uuid
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-COSMOS_CONN = os.environ["CosmosDBConnection"]   
-DB_NAME = "VisitCount"                     
-CONTAINER_NAME = "Visitors"                   
-
 @app.route(route="track", methods=["GET", "POST"])
 def track(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        conn = os.environ["CosmosDBConnection"]
-        client = CosmosClient.from_connection_string(conn)
+        COSMOS_CONN = os.environ["CosmosDBConnection"]   
+        DB_NAME = "VisitCount"                     
+        CONTAINER_NAME = "Visitors"   
+        client = CosmosClient.from_connection_string(COSMOS_CONN)
         database = client.get_database_client(DB_NAME)
         container = database.get_container_client(CONTAINER_NAME)
 
