a5196cc86ce4aa95395eec8b00563e8cc7f384f1
Dominic Senarsky <dominico.sebastian@gmail.com>
2025-10-07 12:21:40 +0200

    Adding the DB code isolated to verify if functional

--- function_app.py
+++ function_app.py
@@ -1,6 +1,16 @@
 import azure.functions as func
+from azure.cosmos import CosmosClient
+import os
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+COSMOS_CONN = os.environ["CosmosDBConnection"]   
+DB_NAME = "VisitCount"                     
+CONTAINER_NAME = "Visitors"                   
+
+client = CosmosClient.from_connection_string(COSMOS_CONN)
+database = client.get_database_client(DB_NAME)
+container = database.get_container_client(CONTAINER_NAME)
 
 @app.route(route="track", methods=["GET", "POST"])
 def track(req: func.HttpRequest) -> func.HttpResponse:
