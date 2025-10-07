d95cb295feda00abe6db2fb83540adbef3b76573
Dominic Senarsky <dominico.sebastian@gmail.com>
2025-10-07 22:59:59 +0200

    gore: losing my mind ever so slowly

--- function_app.py
+++ function_app.py
@@ -1,7 +1,23 @@
 import azure.functions as func
+import uuid
+import json
 
-app = func.FunctionApp()
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="hello", methods=["GET"])
-def hello(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("Hello from Azure Functions!", status_code=200)
+@app.cosmos_db_output(
+    arg_name="doc",
+    database_name="VisitCount",       
+    container_name="Visitors",     
+    connection="CosmosConn"          
+)
+@app.route(route="track", methods=["GET"])
+def track(req: func.HttpRequest, doc: func.Out[str]) -> func.HttpResponse:
+
+    item = {
+        "id": str(uuid.uuid4()),     
+        "note": "Test entry"
+    }
+
+    doc.set(json.dumps(item))
+
+    return func.HttpResponse("Inserted into Cosmos!", status_code=200)
