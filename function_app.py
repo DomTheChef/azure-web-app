a6f110eabd0531d18e8529d0d1dc6dacef949dab
Dominic Senarsky <dominico.sebastian@gmail.com>
2025-10-07 23:50:13 +0200

    test: adding logging to try and identify mismatch

--- function_app.py
+++ function_app.py
@@ -1,26 +1,25 @@
 import azure.functions as func
 import uuid
 import json
+import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.cosmos_db_output(
     arg_name="doc",
-    database_name="VisitCount",       
-    container_name="Visitors",     
-    connection="CosmosConn"          
+    database_name="VisitCount",
+    container_name="Visitors",
+    connection="CosmosConn"
 )
 @app.route(route="track", methods=["GET"])
 def track(req: func.HttpRequest, doc: func.Out[str]) -> func.HttpResponse:
-
-    item = {
-        "id": str(uuid.uuid4()),     
-        "note": "Test entry"
-    }
-
-    doc.set(json.dumps(item))
-
-    return func.HttpResponse("Inserted into Cosmos!", status_code=200)
-
-
-
+    try:
+        item = {
+            "id": str(uuid.uuid4()),
+            "note": "Prod test entry"
+        }
+        doc.set(json.dumps(item))
+        return func.HttpResponse("Inserted into Cosmos!", status_code=200)
+    except Exception as e:
+        logging.error(f"Cosmos DB binding failed: {e}")
+        return func.HttpResponse(f"Cosmos DB binding failed: {e}", status_code=500)
