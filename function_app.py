0e839a9681c7ece53167f4a4d0ada049e3879d6c
Dominic Senarsky <dominico.sebastian@gmail.com>
2025-12-03 00:00:49 +0100

    Chore: Changed cosmos test input, still battling invisible func app.

--- function_app.py
+++ function_app.py
@@ -24,7 +24,7 @@ def track(req: func.HttpRequest) -> func.HttpResponse:
     try:
         item = {
             "id": str(uuid.uuid4()),
-            "note": "Prod test entry",
+            "note": "Dev test entry",
         }
 
         cosmos_container.create_item(item)
